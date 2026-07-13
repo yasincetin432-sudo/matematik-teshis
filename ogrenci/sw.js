9eceb875c8fd27d0eb979f58a0ccc304b8be0a02
yasincetin432-sudo <yasincetin432@gmail.com>
2026-07-14 00:04:03 +0300

    Add files via upload

--- ogrenci/sw.js
+++ ogrenci/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "matapp-v5";
+const CACHE = "matapp-v6";
 self.addEventListener("install", (e) => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
 self.addEventListener("fetch", (e) => {
