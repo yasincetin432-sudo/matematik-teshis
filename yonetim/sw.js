6da81d98453cc9081dcca58aabdcd66edee843f2
yasincetin432-sudo <yasincetin432@gmail.com>
2026-07-16 06:37:41 +0300

    Update sw.js

--- yonetim/sw.js
+++ yonetim/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "matapp-v6";
+const CACHE = "matapp-v7";
 self.addEventListener("install", (e) => self.skipWaiting());
 self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
 self.addEventListener("fetch", (e) => {
